48a15b57330e3248905c2e0d48094aae291d7995
Mideks <MideksYT@gmail.com>
2024-04-25 00:05:47 +0500

    Add typing action

--- routers/searcher.py
+++ routers/searcher.py
@@ -1,3 +1,5 @@
+from asyncio import sleep
+
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
@@ -34,3 +36,13 @@ async def food_list_handler(message: Message, state: FSMContext) -> None:
     products = product_list.split('\n')
     await state.update_data(search_filters=SearchFilters(products))
     await message.delete()
+
+
+@router.callback_query(NavigateButton.filter(F.location == NavigateButtonLocation.StartSearch))
+async def search_handler(callback: CallbackQuery, state: FSMContext) -> None:
+    await state.set_state(None)
+    await callback.message.edit_text(texts.wait_for_result)
+    await callback.message.chat.do('typing')
+    await sleep(3)
+    await callback.message.edit_text(texts.search_result)
+    await callback.answer()
