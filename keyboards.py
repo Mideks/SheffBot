6bba114a9de4984dd29142c661975664bb9c84b7
Mideks <MideksYT@gmail.com>
2024-04-24 22:44:08 +0500

    Add start template

--- keyboards.py
+++ keyboards.py
@@ -7,7 +7,7 @@ from callback_data import NavigateButton, NavigateButtonLocation
 def get_menu_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
-    builder.button(text="🔍👀Давай начнём!",
+    builder.button(text="👀Давай начнём!",
                    callback_data=NavigateButton(location=NavigateButtonLocation.Search))
 
     return builder.as_markup()
